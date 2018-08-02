feb255aa981ed2b428c937c71cfb757c5349025d
XiaochenCui <jcnlcxc.new@gmail.com>
2018-08-02 11:33:26 +0800

    Add install_minikube

---
+++ install_minikube.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+https_proxy=http://127.0.0.1:1087 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-darwin-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
